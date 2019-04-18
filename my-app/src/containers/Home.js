41b64255cec54d8fd365bf0e21bfca05adf53119
lvjie <lvjie@mosainet.com>
2019-04-18 16:01:44 +0800

    练习

--- my-app/src/containers/Home.js
+++ my-app/src/containers/Home.js
@@ -1,60 +1,117 @@
 import React, { Component } from 'react';
-
+import { Button, Table, Alert, message,Modal } from 'antd';
+import '../App.css';
 import { Link } from 'react-router-dom';
 
 class Home extends Component {
-
+    state = {
+        size: 'large',
+        button: '下载按钮'
+    };
+    
+    Delete(record) {
+        console.log(record)
+    }
+    edit = (record) => {
+        console.log(record)
+        this.setState({
+            visible: true,
+        });
+    }
+    handleCancel = () => {
+        this.setState({
+            visible: false,
+        });
+    }
     render() {
         function hidd() {
-            alert(5)
+            message.success('成功提示！')
         }
-        function hidds() {
-            alert('5s')
-        }
-        var data = [
+        const data = [
             {
-                value:0,
-                label:'测试1'
+                value: 0,
+                label: '测试1'
             },
             {
-                value:1,
-                label:'测试2'
+                value: 1,
+                label: '测试2'
             }
         ]
+        const columns = [
+            {
+                title: '值',
+                dataIndex: 'value',
+                key: 'value'
+            },
+            {
+                title: '名称',
+                dataIndex: 'label',
+                key: 'label'
+            },
+            {
+                title: '操作',
+                key: 'action',
+                render: (record) => (
+                    <div>
+                        <Button type="primary" onClick={this.edit.bind(this,record)}>编辑</Button>&nbsp;&nbsp;&nbsp;
+                        <Button onClick={this.Delete.bind(this, record)}>删除</Button>
+                        <Modal
+                            title="Basic Modal"
+                            okText="确定"
+                            cancelText="取消"
+                            visible={this.state.visible}
+                            onCancel={this.handleCancel}
+                        >
+                            <p>Some contents...</p>
+                            <p>Some contents...</p>
+                            <p>Some contents...</p>
+                        </Modal>
+                    </div>
+                )
+            }
+        ]
+        const size = this.state.size;
         return (
             <div>
-                <p id="home-container">
+                <div id="home-container">
                     {/*search,state可以自定义，获取方法：this.props.location.search，this.props.location.state*/}
                     <Link to={{ pathname: '/Test', state: { mold: 'add' }, aa: 'dddd' }} className="home-link">
-                        <button>点击跳转到路由参数search，state使用</button>
+                        <Button>点击跳转到路由参数search，state使用</Button>
                     </Link>
-                </p>
-                <button onClick={hidd}>按钮</button>
-                <button onClick={hidds}>按钮s</button>
+                </div>
+                <p onClick={() => this.props.history.push('/message')}>message页面</p>
+                <Alert message="Success Text" type="success" />,
+                <Button icon="fullscreen" onClick={hidd}>按钮</Button>
+                <Button type="primary" shape="round" icon="download" size={size}>{this.state.button}</Button>
                 <ul>
                     {
-                        data.map(function (item) {
-                            return <li>{item.label}</li>
+                        data.map((item, index) => {
+                            return <li key={index}>{item.label}</li>
+
                         })
                     }
                 </ul>
                 <table>
                     <tr>
                         {
-                            data.map(function (item) {
-                                return <td>{item.value}</td>
+                            data.map((item, index) => {
+                                return <td key={index}>{item.value}</td>
                             })
                         }
 
                     </tr>
                     <tr>
                         {
-                            data.map(function (item) {
-                                return <td>{item.label}</td>
+                            data.map((item, index) => {
+                                return <td key={index}>{item.label}</td>
                             })
                         }
                     </tr>
                 </table>
+                <div className="App">
+                    <Button type="primary">Button</Button>
+                </div>
+                <Table rowKey="value" className="spacing" bordered align="center" dataSource={data} columns={columns} />
             </div>
 
         );
--- my-app/src/containers/Test.js
+++ my-app/src/containers/Test.js
@@ -1,36 +1,55 @@
 import React, { Component } from 'react';
+import { Input, Icon, Form, Button } from 'antd';
+import '../global.css';
+import FormItem from 'antd/lib/form/FormItem';
 
- import { Link } from 'react-router-dom';
+// @Form.create()
 
 class Test extends Component {
+    login = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log(values)
+            }
+        });
+    }
+    render() {
+        const { getFieldDecorator } = this.props.form
+        return (
+            <div>
+                <div id="test-container">
 
-    // constructor (props) {
+                
 
-    //     super(props)
+                </div>
+                <div className="login">
+                    <Form onSubmit={this.login} className="">
+                        <FormItem className="center">
+                            账号登录
+                                </FormItem>
+                        <FormItem>
+                            {
+                                getFieldDecorator('Username', {})(<Input ref={i => this.inputValue = i} prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />)
+                            }
+                        </FormItem>
+                        <FormItem>
+                            {
+                                getFieldDecorator('Password', {})(<Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder="Password" />)
+                            }
+                        </FormItem>
+                        <FormItem className="center">
+                            <Button type="primary" htmlType="submit" className="login-form-button width">
+                                登录
+                                    </Button>
+                        </FormItem>
+                    </Form>
+                </div>
+            </div>
+        );
 
-    // }
-
-   render() {
-
-       return (
-
-           <p id="test-container">
-
-               <p>search:{this.props.location.search} </p>
-
-               <p>state:{this.props.location.state.mold} </p>
-
-               <p onClick={() =>  this.props.history.goBack()}>返回上一页</p>
-               <Link to={{ pathname: '/Message', state: { mold: 'add' }, aa: 'dddd' }}>跳页</Link>
-
-               <p onClick={() => this.props.history.push('/message/12')}>message页面</p>
-
-           </p>
-
-       );
-
-   }
+    }
 
 }
 
-export default Test;
+export default Form.create()(Test);
